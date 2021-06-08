6d361490964cbbb6da04b41abf8b94a710cfb33f
Francesco <gazzola95francy@hotmail.it>
2021-06-08 23:59:58 +0200

    server

--- server.js
+++ server.js
@@ -14,8 +14,66 @@ app.use(express.static("public"));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// listen 
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+  })
 
-app.get('/', (req, res) => {
+// retrieve  schedule from ENSSAT
+
+// import ical
+const ical = require('node-ical');
+
+const config = require("./config.json");
+
+// check if internet conncetion exist 
+// do stuff in an async function if internet connction exists
+;(async () => {
+
+    // retrieve URL from .config file
+    // e.g. URL = 'http://lanyrd.com/topics/nodejs/nodejs.ics'
+    const URL = config.URL;
+
+    // download and parse iCal from the web
+    ical.async.fromURL('http://lanyrd.com/topics/nodejs/nodejs.ics', function(err, data) { 
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        // print on a file to make it available offline or update if it exists
+        console.log(data);
+    });
+})
+
+
+// load file
+app.get('/schedule', (req, res) => {
+
+    // retrieve schedule to load as url parameter
+    const schedule = req.query.schedule;   // ..../schedule?schedule = info
+
+    // look for the file .ics with the selected schedule to load -- > store it in a json file ( use .config.json )
+    const fileSchedule ;
+
+    // open file
+    // retrieve schedule, if found, set fileSchedule
+    // otherwise, ERROR
+    
+    // load and parse this file without blocking the event loop
+    const events = ical.sync.parseFile(fileSchedule);
+
+    // read schedule and display informational
+    // send it to page
+    for (const event of Object.values(events)) {
+        console.log(
+            'Summary: ' + event.summary +
+            '\nDescription: ' + event.description +
+            '\nStart Date: ' + event.start.toISOString() +
+            '\n'
+        );
+    };
+    
+
 })
 
 
