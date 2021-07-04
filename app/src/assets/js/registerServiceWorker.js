0937c237365a90ebd8fa918cd3652e24ab454c8c
Francesco <gazzola95francy@hotmail.it>
2021-07-04 20:58:45 +0200

    Update registerServiceWorker.js

--- app/src/assets/js/registerServiceWorker.js
+++ app/src/assets/js/registerServiceWorker.js
@@ -6,7 +6,11 @@
 
 import { register } from 'register-service-worker'
 
-register(`/service-worker.js`, { // IMPO: the current path is referred to the public folder by default !
+// For avoiding problem in the building process, not set a name different from 'service-worker' for the service worker file
+// This because in the building process a new service worker will be generated with the name 'service-worker'.
+// Hence, setting a different name for the service worker file will cause a no-registration of it after the building process since the file won't be found
+
+register(`/service-worker.js`, { // IMPO: the current path is referred to the public folder by default! 
   ready() {
     console.log(
       'App is being served from cache by a service worker.\n' +
